edf7f81c0116ec4be1272de0423bba0195e3fe28
TerSom <ibnua0291@gmail.com>
2025-10-22 21:17:07 +0700

    belajar javascript lanjutan

---
+++ javascript lanjutan/DestructuringAssignment.js
@@ -0,0 +1,21 @@
+// Destructuring Array
+// const perkenalan = ['halo','nama','saya','terry']
+
+// const [satu,dua,tiga,empat] = perkenalan
+// console.log(satu)
+// console.log(dua)
+
+// skip items
+// const [satu,,,empat] = perkenalan
+// console.log(empat)
+
+// swap itemas
+// let a = 1;
+// let b = 2;
+// console.log(a)
+// console.log(b)
+// let = [a, b] = [b, a];
+// console.log(a)
+// console.log(b)
+
+
