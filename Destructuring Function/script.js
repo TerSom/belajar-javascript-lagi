7237235745b8144fff08291a419eff7889ea48e4
TerSom <ibnua0291@gmail.com>
2025-10-23 19:52:50 +0700

    belajar javscript lanjutan

---
+++ Destructuring Function/script.js
@@ -0,0 +1,39 @@
+// // desctructuring
+// function kalkulasi(a,b) {
+//     return [a + b, a - b ,a * b, a / b ]
+// }
+
+// const [tambah,kurang,kali,bagi] = kalkulasi(2,5)
+// console.log(kurang)
+
+// function kalkulasi(a,b){
+//     return {
+//         tambah : a + b,
+//         kurang : a - b,
+//         kali : a * b,
+//         bagi : a / b
+        
+//     }
+// }
+
+// const {tambah,kali,bagi,kurang} = kalkulasi(2,2)
+// console.log(tambah)
+
+// desctructuring argument
+const mhs1 = {
+    nama : "terry",
+    umur : 17,
+    email : "umaysomay@gmail.com",
+    nilai : {
+        tugas : 80,
+        uts : 90,
+        uas : 92,
+    }
+}
+
+function cetakMhs({nama,umur,email , nilai : {tugas,uts,uas}}){
+    return `halo nama saya ${nama}, umur ${umur} email ${email} dan nilai tugasnya ${tugas}, uts ${uts}, uas ${uas}`
+}
+
+console.log(cetakMhs(mhs1))
+
